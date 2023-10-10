5cebd662f1cfaa9d3d182eed4d2759e284b596b3
Allan Mesquita <allan_santoz@hotmail.com>
2023-10-10 15:07:27 -0300

    Adicionando repositorio local ao remoto

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+I
+src/application/Main.java,8\d\8dd39b27bbecf58d30bde4cb59516e5bb7832dd9
+[
++src/equipamentos/multifuncional/Iphone.java,4\9\494aae7481fda0ae861cc6cb5b7bf245f3ed760b
+b
+2src/equipamentos/telefone/IAparelhoTelefonico.java,9\8\98eb736d115c312e60cf7ed22f6cb970b46761ba
+_
+/src/equipamentos/player/IReprodutorMusical.java,b\3\b38428476fe4498b21e94d61915045632832982d
+b
+2src/equipamentos/navegador/INavegadorInternet.java,b\5\b5438a448ea17b016517f81abac7f63c064c4c9e
---
+++ src/application/Main.java
@@ -0,0 +1,18 @@
+package application;
+import equipamentos.multifuncional.Iphone;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+    new Iphone().adicionarNovaAba();
+    new Iphone().atualizarPagina();
+    new Iphone().exibirPagina();
+    new Iphone().atender();
+    new Iphone().ligar();
+    new Iphone().iniciarCorrerioVoz();
+    new Iphone().tocar();
+    new Iphone().pausar();
+    new Iphone().selecionarMusica();
+    }
+}
